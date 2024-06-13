a29070ec0b0530a5eda39638a47ca0316ccfc9b6
AkagiYamato <akagi-yamato@s-giken.com>
2024-06-13 14:16:57 +0900

    JS実習修正完了

--- index.js
+++ index.js
@@ -14,44 +14,48 @@ function getInputData(){
     //計算結果取得
     const calculationResult = calculation(changedData[0],changedData[1],changedData[2]);
     //計算結果をhtmlに挿入
+    if(calculationResult === null){
+        result.innerText = "計算式が不正です";
+        return;
+    }
     result.innerText = calculationResult;
 }
 
-    function getElement(inputData){
-        const data = inputData.split(' ');
-        let arrayoperator = ["+","-","/","*"];
-        if(data.length !== 3){
-            return null;
-        }
-        const num1 = parseInt(data[0]);
-        const operator = data[1];
-        const num2 = parseInt(data[2]);
-        
-        if(isNaN(num1) || isNaN(num2) || !arrayoperator.includes(operator)){
-           return null;
-        }
-        return[operator,num1,num2];
+function getElement(inputData){
+    const data = inputData.split(' ');
+    
+    if(data.length !== 3){
+        return null;
     }
+    const num1 = parseInt(data[0]);
+    const operator = data[1];
+    const num2 = parseInt(data[2]);
+
+    const arrayoperator = ["+","-","/","*"];
+    if(isNaN(num1) || isNaN(num2) || !arrayoperator.includes(operator)){
+        return null;
+    }
+    return[operator,num1,num2];
+}
     
 
 
-    function calculation (operator,num1,num2){
-        switch(operator){
-            case "+":
-                calculationResult = num1 + num2;
-                return calculationResult;
-            case "-":
-                calculationResult = num1 - num2;
-                return calculationResult;
-            case "*":
-                calculationResult = num1 * num2;
-                return calculationResult;
-            case "/":
-                calculationResult = num1 / num2;
-                return calculationResult;
-            
-            default:
-                return calculationResult = "計算式が不正です";
-        }
+function calculation (operator,num1,num2){
+    switch(operator){
+        case "+":
+            calculationResult = num1 + num2;
+            return calculationResult;
+        case "-":
+            calculationResult = num1 - num2;
+            return calculationResult;
+        case "*":
+            calculationResult = num1 * num2;
+            return calculationResult;
+        case "/":
+            calculationResult = num1 / num2;
+            return calculationResult;
+        default:
+            return null;
     }
+}
 
